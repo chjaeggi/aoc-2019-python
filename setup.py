b73e9f1d364d94b9966b1627c7200d3adbaf166f
Christian Jaeggi <chr.jaeggi@gmail.com>
2019-12-02 18:07:21 +0100

    Init repo for AOC 2019

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Advent.py',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='jaeggi',
+    author_email='chr.jaeggi@gmail.com',
+    description='Advent of Code', install_requires=['']
+)
