fd85bd59c56b4c09adbe9ce599a2645ff98f71ec
Christian Jaeggi <chr.jaeggi@gmail.com>
2019-12-04 19:32:06 +0100

    prepare day5

--- advent.py
+++ advent.py
@@ -4,6 +4,7 @@ from days.day1 import Day1
 from days.day2 import Day2
 from days.day3 import Day3
 from days.day4 import Day4
+from days.day5 import Day5
 
 
 class Advent:
@@ -13,11 +14,13 @@ class Advent:
         self._day2 = Day2().solve(full_quiz=True)
         self._day3 = Day3().solve(full_quiz=True)
         self._day4 = Day4().solve(full_quiz=True)
+        self._day5 = Day5().solve(full_quiz=True)
 
         print(self._day1)
         print(self._day2)
         print(self._day3)
         print(self._day4)
+        print(self._day5)
 
 
 if __name__ == '__main__':
---
+++ days/day5.py
@@ -0,0 +1,9 @@
+class Day5:
+
+    def __init__(self):
+        self._full_quiz = None
+        with open('inputs/04-input.txt') as data:
+            pass
+
+    def solve(self, full_quiz=True):
+        self._full_quiz = full_quiz
