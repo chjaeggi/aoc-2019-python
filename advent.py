a29a45a8bad9e24c79ba12766fbb5f5cb42eb1b4
Christian Jaeggi <chr.jaeggi@gmail.com>
2019-12-06 18:57:47 +0100

    prepare day7

--- advent.py
+++ advent.py
@@ -6,24 +6,27 @@ from days.day3 import Day3
 from days.day4 import Day4
 from days.day5 import Day5
 from days.day6 import Day6
+from days.day7 import Day7
 
 
 class Advent:
 
     def __init__(self):
-        self._day1 = Day1().solve(full_quiz=True)
-        self._day2 = Day2().solve(full_quiz=True)
-        self._day3 = Day3().solve(full_quiz=True)
-        self._day4 = Day4().solve(full_quiz=True)
-        self._day5 = Day5().solve(full_quiz=True)
-        self._day6 = Day6().solve(full_quiz=True)
+        # self._day1 = Day1().solve(full_quiz=True)
+        # self._day2 = Day2().solve(full_quiz=True)
+        # self._day3 = Day3().solve(full_quiz=True)
+        # self._day4 = Day4().solve(full_quiz=True)
+        # self._day5 = Day5().solve(full_quiz=True)
+        # self._day6 = Day6().solve(full_quiz=True)
+        self._day7 = Day7().solve(full_quiz=True)
 
-        print(self._day1)
-        print(self._day2)
-        print(self._day3)
-        print(self._day4)
-        print(self._day5)
-        print(self._day6)
+        # print(self._day1)
+        # print(self._day2)
+        # print(self._day3)
+        # print(self._day4)
+        # print(self._day5)
+        # print(self._day6)
+        print(self._day7)
 
 
 if __name__ == '__main__':
---
+++ days/day7.py
@@ -0,0 +1,12 @@
+from collections import Counter, OrderedDict
+
+
+class Day7:
+
+    def __init__(self):
+        self._input = None
+        with open('inputs/06-input.txt') as data:
+            self._input = data.read().split()
+
+    def solve(self, full_quiz=True):
+        return
