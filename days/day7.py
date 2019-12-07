ac8d50d2c5a81b4e5120ff7de257a13c0858257a
Christian Jaeggi <chr.jaeggi@gmail.com>
2019-12-07 23:47:45 +0100

    day7 part2 solution

--- days/day7.py
+++ days/day7.py
@@ -1,96 +1,118 @@
 from itertools import permutations
 
 
-class Day7:
+class IntCodeMachine:
 
-    def __init__(self):
-        self._original_numbers = None
-        self._highest_output = 0
-        with open('inputs/07-input.txt') as data:
-            self._original_numbers = list(map(int, data.read().split(',')))
+    def __init__(self, codes, phase):
+        self._codes = codes
+        self._phase_setting = phase
+        self._phase_setting_done = False
+        self._pointer = 0
+        self._result = 0
+        self._halted = False
 
-    @staticmethod
-    def _int_code_machine(input_instruction, phase_setting, codes):
-        pointer = 0
-        phase_setting_done = False
-        result = 0
-        while True:
-            op_code = str(codes[pointer]).zfill(5)
+    @property
+    def has_halted(self):
+        return self._halted
+
+    def run(self, instruction_input):
+        while not self._halted:
+            op_code = str(self._codes[self._pointer]).zfill(5)
+
+            if op_code.endswith('99'):
+                self._halted = True
+                return self._result
 
             if not (op_code.endswith('3') or op_code.endswith('4')):
                 param1_poistion_mode = list(op_code)[2] == '0'
                 param2_poistion_mode = list(op_code)[1] == '0'
                 if param1_poistion_mode:
-                    value1 = codes[codes[pointer + 1]]
+                    value1 = self._codes[self._codes[self._pointer + 1]]
                 else:
-                    value1 = codes[pointer + 1]
+                    value1 = self._codes[self._pointer + 1]
                 if param2_poistion_mode:
-                    value2 = codes[codes[pointer + 2]]
+                    value2 = self._codes[self._codes[self._pointer + 2]]
                 else:
-                    value2 = codes[pointer + 2]
+                    value2 = self._codes[self._pointer + 2]
 
             if op_code.endswith('1'):
-                codes[codes[pointer + 3]] = value1 + value2
-                pointer += 4
+                self._codes[self._codes[self._pointer + 3]] = value1 + value2
+                self._pointer += 4
 
             elif op_code.endswith('2'):
-                codes[codes[pointer + 3]] = value1 * value2
-                pointer += 4
+                self._codes[self._codes[self._pointer + 3]] = value1 * value2
+                self._pointer += 4
 
             elif op_code.endswith('3'):
-                if not phase_setting_done:
-                    phase_setting_done = True
-                    codes[codes[pointer + 1]] = phase_setting
+                if not self._phase_setting_done:
+                    self._phase_setting_done = True
+                    self._codes[self._codes[self._pointer + 1]] = self._phase_setting
                 else:
-                    codes[codes[pointer + 1]] = input_instruction
-                pointer += 2
+                    self._codes[self._codes[self._pointer + 1]] = instruction_input
+                self._pointer += 2
 
             elif op_code.endswith('4'):
-                result = codes[codes[pointer + 1]]
-                pointer += 2
+                self._result = self._codes[self._codes[self._pointer + 1]]
+                self._pointer += 2
+                return self._result
 
             elif op_code.endswith('5'):
                 if value1 != 0:
-                    pointer = value2
+                    self._pointer = value2
                 else:
-                    pointer += 3
+                    self._pointer += 3
 
             elif op_code.endswith('6'):
                 if value1 == 0:
-                    pointer = value2
+                    self._pointer = value2
                 else:
-                    pointer += 3
+                    self._pointer += 3
 
             elif op_code.endswith('7'):
                 if value1 < value2:
-                    codes[codes[pointer + 3]] = 1
+                    self._codes[self._codes[self._pointer + 3]] = 1
                 else:
-                    codes[codes[pointer + 3]] = 0
-                pointer += 4
+                    self._codes[self._codes[self._pointer + 3]] = 0
+                self._pointer += 4
 
             elif op_code.endswith('8'):
                 if value1 == value2:
-                    codes[codes[pointer + 3]] = 1
+                    self._codes[self._codes[self._pointer + 3]] = 1
                 else:
-                    codes[codes[pointer + 3]] = 0
-                pointer += 4
-
-            elif op_code.endswith('99'):
-                return result
+                    self._codes[self._codes[self._pointer + 3]] = 0
+                self._pointer += 4
 
             else:
                 print("Error, breaking now")
                 break
 
-    def solve(self, full_quiz=True):
-        input_instruction = 0
-        possible_sequences = list(permutations([0, 1, 2, 3, 4]))
-        for possible_sequence in possible_sequences:
-            for element in possible_sequence:
-                input_instruction = self._int_code_machine(input_instruction, element, self._original_numbers)
 
-            if self._highest_output < input_instruction:
-                self._highest_output = input_instruction
-            input_instruction = 0
+class Day7:
+
+    def __init__(self):
+        self._original_numbers = None
+        self._highest_output = 0
+        self._global_input = 0
+        with open('inputs/07-input.txt') as data:
+            self._original_numbers = list(map(int, data.read().split(',')))
+
+    def solve(self, full_quiz=True):
+        possible_sequences = list(permutations([5, 6, 7, 8, 9]))
+        for possible_sequence in possible_sequences:
+            int_code_machines = []
+            for index, element in enumerate(possible_sequence):
+                int_code_machines.append(IntCodeMachine(self._original_numbers, element))
+
+            current_index = 0
+            while not int_code_machines[4].has_halted:
+                self._global_input = int_code_machines[current_index].run(self._global_input)
+                if current_index < 4:
+                    current_index += 1
+                else:
+                    current_index = 0
+
+            if self._highest_output < self._global_input:
+                self._highest_output = self._global_input
+            self._global_input = 0
 
         return self._highest_output
