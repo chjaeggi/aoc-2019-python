c2b59d0d3fee8abe2f4c07cbce73b84eb254b320
Christian Jaeggi <chr.jaeggi@gmail.com>
2019-12-09 20:51:06 +0100

    rename to address (what it actually is)

--- days/day9/int_code_machine.py
+++ days/day9/int_code_machine.py
@@ -14,19 +14,19 @@ class IntCodeMachine:
     def has_halted(self):
         return self._halted
 
-    def _get_command_arguments(self, op_code):
+    def _get_argument_addresses(self, op_code):
 
         param_modes = [list(op_code)[2], list(op_code)[1], list(op_code)[0]]
-        params = [''] * 3
+        addresses = [''] * 3
 
         for idx in range(len(param_modes)):
             if param_modes[idx] == self.POSITION_MODE:
-                params[idx] = self._codes[self._pointer + 1 + idx]
+                addresses[idx] = self._codes[self._pointer + 1 + idx]
             elif param_modes[idx] == self.IMMEDIATE_MODE:
-                params[idx] = self._pointer + 1 + idx
+                addresses[idx] = self._pointer + 1 + idx
             else:
-                params[idx] = self._relative_base + self._codes[self._pointer + 1 + idx]
-        return params[0], params[1], params[2]
+                addresses[idx] = self._relative_base + self._codes[self._pointer + 1 + idx]
+        return addresses[0], addresses[1], addresses[2]
 
     def run(self, instruction_input):
         while not self._halted:
@@ -36,53 +36,53 @@ class IntCodeMachine:
                 self._halted = True
                 return self._result
 
-            param1, param2, param3 = self._get_command_arguments(op_code)
+            arg1_address, arg2_address, arg3_address = self._get_argument_addresses(op_code)
 
             if op_code.endswith('1'):
-                self._codes[param3] = self._codes[param1] + self._codes[param2]
+                self._codes[arg3_address] = self._codes[arg1_address] + self._codes[arg2_address]
                 self._pointer += 4
 
             elif op_code.endswith('2'):
-                self._codes[param3] = self._codes[param1] * self._codes[param2]
+                self._codes[arg3_address] = self._codes[arg1_address] * self._codes[arg2_address]
                 self._pointer += 4
 
             elif op_code.endswith('3'):
-                self._codes[param1] = instruction_input
+                self._codes[arg1_address] = instruction_input
                 self._pointer += 2
 
             elif op_code.endswith('4'):
-                self._result = self._codes[param1]
+                self._result = self._codes[arg1_address]
                 self._pointer += 2
                 print(self._result)
 
             elif op_code.endswith('5'):
-                if self._codes[param1] != 0:
-                    self._pointer = self._codes[param2]
+                if self._codes[arg1_address] != 0:
+                    self._pointer = self._codes[arg2_address]
                 else:
                     self._pointer += 3
 
             elif op_code.endswith('6'):
-                if self._codes[param1] == 0:
-                    self._pointer = self._codes[param2]
+                if self._codes[arg1_address] == 0:
+                    self._pointer = self._codes[arg2_address]
                 else:
                     self._pointer += 3
 
             elif op_code.endswith('7'):
-                if self._codes[param1] < self._codes[param2]:
-                    self._codes[param3] = 1
+                if self._codes[arg1_address] < self._codes[arg2_address]:
+                    self._codes[arg3_address] = 1
                 else:
-                    self._codes[param3] = 0
+                    self._codes[arg3_address] = 0
                 self._pointer += 4
 
             elif op_code.endswith('8'):
-                if self._codes[param1] == self._codes[param2]:
-                    self._codes[param3] = 1
+                if self._codes[arg1_address] == self._codes[arg2_address]:
+                    self._codes[arg3_address] = 1
                 else:
-                    self._codes[param3] = 0
+                    self._codes[arg3_address] = 0
                 self._pointer += 4
 
             elif op_code.endswith('9'):
-                self._relative_base += self._codes[param1]
+                self._relative_base += self._codes[arg1_address]
                 self._pointer += 2
 
             else:
